0860cd095930affbeda167433f1f3a93bb1e9d5d
Ziad Mostafa <mohammedziad.badr@gmail.com>
2023-05-14 12:30:30 +0200

    format file
    Signed-off-by: Ziad Mostafa <mohammedziad.badr@gmail.com>

--- src/concurrency-in-action/ch01/hello-concurrent-world.cpp
+++ src/concurrency-in-action/ch01/hello-concurrent-world.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <thread>
 
-void hello()
-{
-    std::cout << "Hello Concurrent world .. ! " << std::endl;
-}
+void hello() { std::cout << "Hello Concurrent world .. ! " << std::endl; }
 
 int main()
 {
