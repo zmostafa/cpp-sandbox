0ff44d17439746cf8f5f8078c8618d393dda8352
Ziad Mostafa <mohammedziad.badr@gmail.com>
2023-05-14 22:36:33 +0200

    add hierarchical mutex implementation
    Signed-off-by: Ziad Mostafa <mohammedziad.badr@gmail.com>

---
+++ src/concurrency-in-action/ch03/hierarchical-mutex.cpp
@@ -0,0 +1,48 @@
+#include <mutex>
+#include <stdexcept>
+
+class hierarchical_mutex
+{
+  std::mutex internal_mutex;
+  unsigned long const hierarchy_value;
+  unsigned long previous_hierarchy_value; // NOLINT
+  static thread_local unsigned long this_thread_hirearchy_value; // NOLINT
+
+  void check_for_hierarchy_violationn() const
+  {
+    if (this_thread_hirearchy_value <= hierarchy_value) { throw std::logic_error("mutex hierarchy violated"); }
+  }
+
+  void update_hierarchy_value()
+  {
+    previous_hierarchy_value = this_thread_hirearchy_value;
+    this_thread_hirearchy_value = hierarchy_value;
+  }
+
+public:
+  explicit hierarchical_mutex(unsigned long value) : hierarchy_value(value), previous_hierarchy_value(0) {}
+
+  void lock()
+  {
+    check_for_hierarchy_violationn();
+    internal_mutex.lock();
+    update_hierarchy_value();
+  }
+
+  void unlock()
+  {
+    if (this_thread_hirearchy_value != hierarchy_value) { throw std::logic_error("mutex hirearchy violated"); }
+    this_thread_hirearchy_value = previous_hierarchy_value;
+    internal_mutex.unlock();
+  }
+
+  bool try_lock()
+  {
+    check_for_hierarchy_violationn();
+    if (!internal_mutex.try_lock()) { return false; }
+    update_hierarchy_value();
+    return true;
+  }
+};
+
+thread_local unsigned long hierarchical_mutex::this_thread_hirearchy_value(ULONG_MAX); // NOLINT
