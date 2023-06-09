3577d315043e1e3acf5674ab4aa6115b1780268b
Ziad Mostafa <mohammedziad.badr@gmail.com>
2023-06-09 17:43:12 +0200

    add comments
    Signed-off-by: Ziad Mostafa <mohammedziad.badr@gmail.com>

--- src/concurrency-in-action/ch04/conditional-variable.cpp
+++ src/concurrency-in-action/ch04/conditional-variable.cpp
@@ -15,6 +15,8 @@ void data_preparation_thread()
 {
   while (more_data_to_prepare()) {
     data_chunk const data = prepare_data();
+    // Put the code for adding the data in a small scope
+    // then notify the condition_variable after unlocking
     {
       std::lock_guard<std::mutex> lk(mut);
       data_queue.push(data);
@@ -26,6 +28,8 @@ void data_preparation_thread()
 void data_processing_thread()
 {
   while (true) {
+    // unique_lock is used to enable the thread to release mutex
+    // if the condition in wait() is not fulfilled, lock_guard won't
     std::unique_lock<std::mutex> lk(mut);
     data_cond.wait(lk, [] { return !data_queue.empty(); });
     data_chunk data = data_queue.front();
